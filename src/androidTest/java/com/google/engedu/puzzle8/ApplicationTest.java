71b319635ad71edad82c2601da5d5296f527579f
Nishit Tated <nishittated@gmail.com>
2018-06-03 07:37:08 +0530

    Update ApplicationTest.java

--- src/androidTest/java/com/google/engedu/puzzle8/ApplicationTest.java
+++ src/androidTest/java/com/google/engedu/puzzle8/ApplicationTest.java
@@ -1,7 +1,7 @@
 package com.google.engedu.puzzle8; 
 
 import android.app.Application;  
-import android.test.ApplicationTestCase;
+import android.test.ApplicationTestCase; 
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
